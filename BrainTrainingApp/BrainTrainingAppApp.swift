009771b4ea5dadff6b0327791b1ba0730686c993
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-03-03 16:00:01 -0800

    Initial Commit

---
+++ BrainTrainingApp/BrainTrainingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainingAppApp.swift
+//  BrainTrainingApp
+//
+//  Created by Arseniy Oksenoyt on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
